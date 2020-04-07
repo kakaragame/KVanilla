2c1478b9638c9fac29de64be2494f724b58808ce
wherkamp <wherkamp@kingtux.me>
2020-04-07 16:09:59 -0400

    Update chunk Generation

--- mod/src/main/java/org/kakara/kvanilla/mod/generators/normal/NormalChunkGenerator.java
+++ mod/src/main/java/org/kakara/kvanilla/mod/generators/normal/NormalChunkGenerator.java
@@ -17,6 +17,7 @@ import java.util.Random;
 @Id("default")
 public class NormalChunkGenerator extends ModChunkGenerator {
     private Mod mod;
+
     public NormalChunkGenerator(Mod mod) {
         super(mod);
         this.mod = mod;
@@ -26,6 +27,9 @@ public class NormalChunkGenerator extends ModChunkGenerator {
     public ChunkBase generateChunk(int seed, Random r, ChunkBase chunkBase) {
         NoiseGenerator n1 = new NoiseGenerator(seed, 0.05F);
         NoiseGenerator n2 = new NoiseGenerator(r.nextInt(), 0.1F);
+        Item grassDirt = Kakara.getItemManager().getItem(new NameKey(mod.getName(), "grassy_dirt"));
+        Item dirt = Kakara.getItemManager().getItem(new NameKey(mod.getName(), "dirt"));
+        Item stone = Kakara.getItemManager().getItem(new NameKey(mod.getName(), "stone"));
         for (int x = chunkBase.getX(); x < 16 + chunkBase.getX(); x++) {
             for (int y = chunkBase.getY(); y < 16 + chunkBase.getY(); y++) {
                 for (int z = chunkBase.getZ(); z < 16 + chunkBase.getZ(); z++) {
@@ -35,18 +39,17 @@ public class NormalChunkGenerator extends ModChunkGenerator {
 
                     Item item;
                     if (y == groundHeight) {
-                        item = Kakara.getItemManager().getItem(new NameKey(mod.getName(), "grassy_dirt"));
+                        item = grassDirt;
                         if (item == null) System.out.println("null Grassy");
                     } else if (y > groundHeight) {
                         continue;
                     } else if (y > groundHeight - 5) {
-                        item = Kakara.getItemManager().getItem(new NameKey(mod.getName(), "dirt"));
+                        item = dirt;
                         if (item == null) System.out.println("null dirt");
 
                     } else {
-                        item = Kakara.getItemManager().getItem(new NameKey(mod.getName(), "stone"));
+                        item = stone;
                         if (item == null) System.out.println("null stone");
-
                     }
 
                     chunkBase.setBlock(x, y, z, Kakara.createItemStack(item));
