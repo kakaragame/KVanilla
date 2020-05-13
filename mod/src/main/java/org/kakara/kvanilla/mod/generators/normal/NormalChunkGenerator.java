15beb90365f869a2cb97c167b8a927280f9ca1a1
wherkamp <wherkamp@kingtux.me>
2020-05-12 20:03:44 -0400

    Made it compile(Dont use just .get)

--- mod/src/main/java/org/kakara/kvanilla/mod/generators/normal/NormalChunkGenerator.java
+++ mod/src/main/java/org/kakara/kvanilla/mod/generators/normal/NormalChunkGenerator.java
@@ -26,9 +26,9 @@ public class NormalChunkGenerator extends ModChunkGenerator {
     @Override
     public ChunkBase generateChunk(int seed, Random r, ChunkBase chunkBase) {
         NoiseGenerator n1 = new NoiseGenerator(seed, 0.05F);
-        ItemStack grassDirt = Kakara.createItemStack(Kakara.getItemManager().getItem(new NameKey(mod.getName(), "grassy_dirt")));
-        ItemStack dirt = Kakara.createItemStack(Kakara.getItemManager().getItem(new NameKey(mod.getName(), "dirt")));
-        ItemStack stone = Kakara.createItemStack(Kakara.getItemManager().getItem(new NameKey(mod.getName(), "stone")));
+        ItemStack grassDirt = Kakara.createItemStack(Kakara.getItemManager().getItem(new NameKey(mod.getName(), "grassy_dirt")).get());
+        ItemStack dirt = Kakara.createItemStack(Kakara.getItemManager().getItem(new NameKey(mod.getName(), "dirt")).get());
+        ItemStack stone = Kakara.createItemStack(Kakara.getItemManager().getItem(new NameKey(mod.getName(), "stone")).get());
 
         for (int x = chunkBase.getX(); x < 16 + chunkBase.getX(); x++) {
             for (int z = chunkBase.getZ(); z < 16 + chunkBase.getZ(); z++) {
