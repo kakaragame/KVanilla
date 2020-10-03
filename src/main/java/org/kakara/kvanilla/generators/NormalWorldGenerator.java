9301dca3ccc2bceaecbf54775161b4491652b6ea
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2020-10-03 12:37:27 -0400

    KT-Rewrite

--- src/main/java/org/kakara/kvanilla/generators/NormalWorldGenerator.java
+++ src/main/java/org/kakara/kvanilla/generators/NormalWorldGenerator.java
@@ -1,15 +1,14 @@
 package org.kakara.kvanilla.generators;
 
 import org.kakara.core.Kakara;
-import org.kakara.core.NameKey;
 import org.kakara.core.annotations.Key;
 import org.kakara.core.annotations.Name;
 import org.kakara.core.game.Item;
-import org.kakara.core.mod.Mod;
 import org.kakara.core.mod.game.ModWorldGenerator;
 import org.kakara.core.world.ChunkBase;
 import org.kakara.core.world.World;
 import org.kakara.core.world.region.RegionGrid;
+import org.kakara.kvanilla.KVanillaMain;
 import org.kakara.kvanilla.generators.noise.PerlinNoise;
 import org.kakara.kvanilla.regions.KRegionGrid;
 
@@ -22,12 +21,12 @@ public class NormalWorldGenerator extends ModWorldGenerator {
     Item grassDirt, dirt, stone, infinityStone;
     PerlinNoise noise;
 
-    public NormalWorldGenerator(Mod mod) {
+    public NormalWorldGenerator(KVanillaMain mod) {
         super(mod);
-        grassDirt = Kakara.getItemManager().getItem(new NameKey(mod, "grassy_dirt")).get();
-        dirt = Kakara.getItemManager().getItem(new NameKey(mod, "dirt")).get();
-        stone = Kakara.getItemManager().getItem(new NameKey(mod, "stone")).get();
-        infinityStone = Kakara.getItemManager().getItem(new NameKey(mod, "infinity_stone")).get();
+        grassDirt = Kakara.getItemManager().getItem(mod.createControllerKey("grassy_dirt"));
+        dirt = Kakara.getItemManager().getItem(mod.createControllerKey("dirt"));
+        stone = Kakara.getItemManager().getItem(mod.createControllerKey("stone"));
+        infinityStone = Kakara.getItemManager().getItem(mod.createControllerKey("infinity_stone"));
         noise = new PerlinNoise();
     }
 
